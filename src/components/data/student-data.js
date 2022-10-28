9fb84b4905c6ab149a15a53c41bcca130848f7fb
SabinePakes <102619317+SabinePakes@users.noreply.github.com>
2022-10-28 15:14:15 +0200

    Create student-data.js

---
+++ src/components/data/student-data.js
@@ -0,0 +1,95 @@
+const students = [
+    {
+      id: 1,
+      first_name: 'Evelyn',
+      last_name: 'Roughley',
+      email: 'droughley0@abc.net.au',
+      phone: '706-434-6401',
+      age: 1994,
+      photo: 'https://randomuser.me/api/portraits/women/81.jpg'
+    },
+    {
+      id: 2,
+      first_name: 'Aranka',
+      last_name: 'Straniero',
+      email: 'cstraniero1@nationalgeographic.com',
+      phone: '768-141-8881',
+      age: 1999,
+      photo: 'https://randomuser.me/api/portraits/women/3.jpg'
+    },
+    {
+      id: 3,
+      first_name: 'Floris',
+      last_name: 'Barrie',
+      email: 'cbarrie2@businessinsider.com',
+      phone: '443-667-5152',
+      age: 1992,
+      photo: 'https://randomuser.me/api/portraits/men/58.jpg'
+    },
+    {
+      id: 4,
+      first_name: 'Hector',
+      last_name: 'Fallowfield',
+      email: 'lfallowfield3@1und1.de',
+      phone: '251-709-4443',
+      age: 2007,
+      photo: 'https://randomuser.me/api/portraits/men/21.jpg'
+    },
+    {
+      id: 5,
+      first_name: 'Martina',
+      last_name: 'Kilrow',
+      email: 'fkilrow4@squidoo.com',
+      phone: '148-957-5087',
+      age: 2000,
+      photo: 'https://randomuser.me/api/portraits/women/48.jpg'
+    },
+    {
+      id: 6,
+      first_name: 'Maurits',
+      last_name: 'Edmundson',
+      email: 'cedmundson5@gmpg.org',
+      phone: '232-623-8107',
+      age: 2011,
+      photo: 'https://randomuser.me/api/portraits/men/87.jpg'
+    },
+    {
+      id: 7,
+      first_name: 'Rahima',
+      last_name: 'Hosburn',
+      email: 'thosburn6@tinypic.com',
+      phone: '503-157-5306',
+      age: 2002,
+      photo: 'https://randomuser.me/api/portraits/women/59.jpg'
+    },
+    {
+      id: 8,
+      first_name: 'Sandra',
+      last_name: 'Sibbald',
+      email: 'gsibbald7@shinystat.com',
+      phone: '738-354-9264',
+      age: 1986,
+      photo: 'https://randomuser.me/api/portraits/women/75.jpg'
+    },
+    {
+      id: 9,
+      first_name: 'Wietske',
+      last_name: 'Ridwood',
+      email: 'eridwood8@cdc.gov',
+      phone: '370-294-0756',
+      age: 2007,
+      photo: 'https://randomuser.me/api/portraits/women/85.jpg'
+    },
+    {
+      id: 10,
+      first_name: 'Storm',
+      last_name: 'Nott',
+      email: 'mnott9@chicagotribune.com',
+      phone: '789-709-6779',
+      age: 1998,
+      photo: 'https://randomuser.me/api/portraits/women/71.jpg'
+    }
+  ]
+  
+  export default students
+  
